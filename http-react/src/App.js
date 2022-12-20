c9acb279aa3e98b36eb5ac8cdf31febe2f1febd1
Yuri <yurifdev@gmail.com>
2022-12-19 22:33:35 -0300

    Creating the fetch as react suggests

--- http-react/src/App.js
+++ http-react/src/App.js
@@ -1,31 +1,38 @@
-import './App.css';
-import React from 'react'
+import "./App.css";
+import React from "react";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 const url = "http://localhost:3000/products";
 
 function App() {
   //Aula 1
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
   //Resgatando dados
   useEffect(() => {
     async function fetchData() {
-       const res = await fetch(url)
+      const res = await fetch(url);
 
-      const data = await res.json()
+      const data = await res.json();
 
-      setProducts(data)
+      setProducts(data);
     }
 
-    fetchData()
-  }, []); 
+    fetchData();
+  }, []);
   //Nao há nenhuma depedência específica aqui
-  
+
   console.log(products);
   return (
     <div>
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
