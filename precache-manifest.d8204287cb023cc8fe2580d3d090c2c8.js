8a94e809c5a7e0f6f47be46ae45e01905d1c92bc
Yuri <yurifdev@gmail.com>
2023-01-16 19:51:33 -0300

    Updates

---
+++ precache-manifest.d8204287cb023cc8fe2580d3d090c2c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "139ff9681dd5a2f7677b",
+    "url": "/Proj.Study-React-7/static/css/main.97b5d502.chunk.css"
+  },
+  {
+    "revision": "139ff9681dd5a2f7677b",
+    "url": "/Proj.Study-React-7/static/js/main.139ff968.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/Proj.Study-React-7/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "9ce0b56ff27aff6ffc27",
+    "url": "/Proj.Study-React-7/static/js/2.9ce0b56f.chunk.js"
+  },
+  {
+    "revision": "6e19dbee4de243a4c3c1",
+    "url": "/Proj.Study-React-7/static/js/runtime~main.6e19dbee.js"
+  },
+  {
+    "revision": "84a9cf1645853842657aa009d0409539",
+    "url": "/Proj.Study-React-7/index.html"
+  }
+];
